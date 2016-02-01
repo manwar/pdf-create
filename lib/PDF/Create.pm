69cf117e212ed8377851ad775bed2dcd2a1b30f1
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2016-02-01 16:10:31 +0000

    - Removed +x attribute from some of the test scripts. - Added test scripts t/meta-json.t and t/meta-yml.t.

--- lib/PDF/Create.pm
+++ lib/PDF/Create.pm
@@ -1,6 +1,6 @@
 package PDF::Create;
 
-our $VERSION = '1.27';
+our $VERSION = '1.28';
 
 =head1 NAME
 
@@ -8,7 +8,7 @@ PDF::Create - Create PDF files.
 
 =head1 VERSION
 
-Version 1.27
+Version 1.28
 
 =cut
 
