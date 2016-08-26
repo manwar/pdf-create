72907490417057e5adef6808b96aced1bb07bd97
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2016-08-26 08:41:04 +0100

    - Corrected HelveticaBold font width.

--- lib/PDF/Create.pm
+++ lib/PDF/Create.pm
@@ -1,6 +1,6 @@
 package PDF::Create;
 
-our $VERSION = '1.33';
+our $VERSION = '1.34';
 
 =head1 NAME
 
@@ -8,7 +8,7 @@ PDF::Create - Create PDF files.
 
 =head1 VERSION
 
-Version 1.33
+Version 1.34
 
 =cut
 
--- lib/PDF/Font.pm
+++ lib/PDF/Font.pm
@@ -1,6 +1,6 @@
 package PDF::Font;
 
-our $VERSION = '1.33';
+our $VERSION = '1.34';
 
 =head1 NAME
 
@@ -8,7 +8,7 @@ PDF::Font - Base font class for PDF::Create.
 
 =head1 VERSION
 
-Version 1.33
+Version 1.34
 
 =cut
 
